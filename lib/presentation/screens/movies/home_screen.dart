e41511302effb8f4d54c1b893b95f60a0be63d23
rsorianosanchis <rsorianosanchis.developer@gmail.com>
2023-08-07 20:22:06 +0200

    basic arquitecture for basic StNotProv and consumers  done

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
-import 'package:biopedia_23/config/constants/environment.dart';
+import 'package:biopedia_23/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home-screen';
@@ -8,8 +9,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: Placeholder()
+    return const Scaffold(body: _HomeView());
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  //dentro del consumer state tengo acceso al ref
+  //nota: acordarse que dentro de los metodos usamos el ref.read
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMovies.isEmpty) const CircularProgressIndicator();
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        return ListTile(title: Text(nowPlayingMovies[index].title));
+      },
     );
   }
 }
